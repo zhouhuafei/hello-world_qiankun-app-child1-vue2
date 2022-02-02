4729fb9f14e6378bb00924fc861ab9804cd5d4d1
zhouhuafei <1123486116@qq.com>
2022-02-02 20:01:48 +0800

    feat: 🎸 微前端 - qiankuan - child1-vue2

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
+const { name } = require('./package')
 module.exports = {
-  devServer: { port: 7071 },
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    port: 7071,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  },
+  configureWebpack: {
+    output: {
+      library: `${name}-[name]`,
+      libraryTarget: 'umd', // 把微应用打包成 umd 库格式
+      jsonpFunction: `webpackJsonp_${name}`
+    }
+  }
 }
